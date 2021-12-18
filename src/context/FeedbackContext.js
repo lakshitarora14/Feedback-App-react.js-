0ae5512a31b5dec4893d753bbdacae51c98a7bef
= <=>
2021-12-18 16:54:49 +0530

    test

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -3,4 +3,4 @@ import { createContext, useState } from 'react'
 const FeedbackContext = createContext()
 
 // to get our components access to all the states and functions we will wrap everythinf inside a provider
-// this is same like we wrap everything inside a router
+// this is same like we wrap everything inside a router 
